d980cd3db13d3e6b5ed16753239d07f1105a1f01
David Macintosh (Dawid Z. D.) <dawid9554@gmail.com>
2019-11-01 20:32:34 +0100

    Create siema.c

---
+++ entries/siema.c
@@ -0,0 +1,16 @@
+char*m="@236606117033!#67767198523508991"
+"*--                                 --*"
+"*--  bCB  EAEAA CABCB A   A  ACA  C --*"
+"*-- A   C   A   E     BA BA A   C A --*"
+"*-- A       A   A     A G A C   A E --*"
+"*--  AAA    A   AAAC  A   A ABCDA C --*"
+"*--     D   B   A     C   A A   E A --*"
+"*-- A   E   A   C     A   C A   E   --*"
+"*--  ACA  AACAA AAACA C   A C   A E --*"
+"*--                                 --*"
+"*'ACAA007ACADCBEAE2A618ACA!A9A900CAAA`*"
+
+;main(){int z=0,k=0,p=1;while(m[p]){if(m[
+p]>m[0]){for(char i=0;i<-m[0]+m[p];i+=1){
+putchar(k?219:32);if(!((++z)%32))putchar(
+10);}k=!k;}p++;}}/*2019 David Macintosh*/

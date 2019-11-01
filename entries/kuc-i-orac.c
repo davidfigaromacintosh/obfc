63fb11adc7f46a586d5fe35f213595f089a806f5
David Macintosh (Dawid Z. D.) <dawid9554@gmail.com>
2019-11-01 20:31:57 +0100

    Create kuc-i-orac.c

---
+++ entries/kuc-i-orac.c
@@ -0,0 +1,38 @@
+main(){printf("%.3lf\n",OwO*TwT(UwU)/TwT(!UwU));}TwT(_){return(
+/*3.14159265358979323846*/#define O+1/*2643383279502884197169*/
+/*3993751058209749445923*/#define _+_/*0781640628620899862803*/
+_ _ _ _ _ _ _ _ _ _ _ _ O O O O O O O O _ _ _ _ _ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _ _ O O O O O O O O O O O O O O _ _ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ O O O O O O O O O O O O O O O O O O _ _ _ _ _ _ _
+_ _ _ _ _ _ O O O O O O O O O O O O O O O O O O O O _ _ _ _ _ _
+_ _ _ _ _ O O O O O O O O O O O O O O O O O O O O O O _ _ _ _ _
+_ _ _ _ O O O O O O O O O O O O O O O O O O O O O O O O _ _ _ _
+_ _ _ O O O O O O O O O O O O O O O O O O O O O O O O O O _ _ _
+_ _ O O O O O O O O O O O O O O O O O O O O O O O O O O O O _ _
+_ _ O O O O O O O O O O O O O O O O O O O O O O O O O O O O _ _
+_ O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O _
+_ O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O _
+_ O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O _
+O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
+O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
+O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
+O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
+O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
+O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
+O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
+O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O
+_ O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O _
+_ O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O _
+_ O O O O O O O O O O O O O O O O O O O O O O O O O O O O O O _
+_ _ O O O O O O O O O O O O O O O O O O O O O O O O O O O O _ _
+_ _ O O O O O O O O O O O O O O O O O O O O O O O O O O O O _ _
+_ _ _ O O O O O O O O O O O O O O O O O O O O O O O O O O _ _ _
+_ _ _ _ O O O O O O O O O O O O O O O O O O O O O O O O _ _ _ _
+_ _ _ _ _ O O O O O O O O O O O O O O O O O O O O O O _ _ _ _ _
+_ _ _ _ _ _ O O O O O O O O O O O O O O O O O O O O _ _ _ _ _ _
+_ _ _ _ _ _ _ O O O O O O O O O O O O O O O O O O _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _ _ O O O O O O O O O O O O O O _ _ _ _ _ _ _ _ _
+_ _ _ _ _ _ _ _ _ _ _ _ O O O O O O O O _ _ _ _ _ _ _ _ _ _ _ _
+);/*KUĆ I ORAĆ W DZIEŃ ZAWZIĘCIE, BO PLANÓW NIEMA BEZ TRUDU*/;}
+
+//gcc src.c -o dest -DOwO=4. -DUwU=0
